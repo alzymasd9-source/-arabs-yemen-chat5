cb86e55b531e0376f6c00ea8a0028ea7c1ff58c2
alzymasd9-source <alzymasd9@gmail.com>
2026-06-25 04:25:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,23 +16,39 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// دالة التاريخ والوقت: يوم/شهر ساعة:دقيقة ص/م
+function getDateNow(){
+  const d = new Date();
+  const day = d.getDate();
+  const month = d.getMonth() + 1;
+  let hours = d.getHours();
+  const minutes = d.getMinutes().toString().padStart(2,'0');
+  const ampm = hours >= 12 ? 'م' : 'ص';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  return `${day}/${month} ${hours}:${minutes} ${ampm}`;
+}
+
 io.on('connection', (socket) => {
   console.log('مستخدم اتصل: ' + socket.id);
   
   socket.on('join', (data) => {
     socket.username = data.name;
     console.log(data.name + ' دخل');
-    io.emit('chat message', {name: 'النظام', text: `${data.name} دخل الشات`});
+    // رسالة النظام بدون وقت وتاريخ
+    io.emit('chat message', {name: 'النظام', text: `👋 ${data.name} انضم للغرفة`});
   });
 
   socket.on('chat message', (data) => {
     console.log(data.name + ': ' + data.text);
-    io.emit('chat message', data);
+    // الرسالة العادية مع الوقت والتاريخ
+    io.emit('chat message', {name: data.name, text: data.text, time: getDateNow()});
   });
 
   socket.on('disconnect', () => {
     if(socket.username){
-      io.emit('chat message', {name: 'النظام', text: `${socket.username} خرج`});
+      // رسالة النظام بدون وقت وتاريخ
+      io.emit('chat message', {name: 'النظام', text: `👋 ${socket.username} غادر الغرفة`});
     }
   });
 });
