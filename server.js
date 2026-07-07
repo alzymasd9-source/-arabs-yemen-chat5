7a323a1376cfd1abf43c80f350547eca84e73277
alzymasd9-source <alzymasd9@gmail.com>
2026-07-07 16:44:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,25 +7,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// تحديد المجلد الذي يحتوي على ملفات الواجهة الأمامية
 app.use(express.static(path.join(__dirname, 'public')));
 
-// إدارة اتصالات الشات
 io.on('connection', (socket) => {
     console.log('مستخدم جديد اتصل بالشات');
 
-    // استقبال الرسالة من مستخدم وإرسالها للجميع
-    socket.on('chatMessage', (msg) => {
-        io.emit('chatMessage', msg);
+    // استقبال كائن يحتوي على الاسم والرسالة وإعادة توجيهه للجميع
+    socket.on('chatMessage', (data) => {
+        io.emit('chatMessage', data); 
     });
 
-    // عند خروج المستخدم
     socket.on('disconnect', () => {
         console.log('مستخدم غادر الشات');
     });
 });
 
-// تشغيل السيرفر على المنفذ 3000
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`السيرفر يعمل على الرابط: http://localhost:${PORT}`);
